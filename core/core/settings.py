c9bc395cca1ec0aeb710d37dfa12af5db17899c9
cristhian-soto-j <cristhian.soto.j@gmail.com>
2023-10-11 02:13:20 -0300

    1

--- core/core/settings.py
+++ core/core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'db': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db',
         'USER': 'kri',
